7949c5c11fcf6033845bc16156ab6a44b3f3ad63
Víctor <vesqgar3008@g.educaand.es>
2022-11-30 08:10:20 +0100

    RadioAficionados:MainPageBoceto

--- Views/default/body.php
+++ Views/default/body.php
@@ -3,3 +3,22 @@
     // $rp = new repConcurso($con);
     // $rp->get_concursos;
 ?>
+<div class="Quienes">
+    <h2>¿Quiénes somos?</h2>
+    <p>
+        Somos una asociación libre de radioaficionados
+        que centramos nuestros esfuerzos y recursos en 
+        organizar concursos en España.
+        Los miembros de nuestra organización pagan una cuota
+        a cambio de participar en los concursos que se organizan
+        con dicho dinero.
+    </p>
+</div>
+<div class="c-QyA">
+<!-- Preguntas frecuentess -->
+¿A dónde va el dinero de mi suscripción?
+Todo el dinero recaudado se emplea en el mantenimiento de esta página
+y la organización de concursos que son publicados en ésta.
+También invertimos parte de la recaudación para cestas regalo y otros
+eventos organizados con nuestros suscriptores.
+</div>
